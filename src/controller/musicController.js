6f4b75aa2de0964eef7b70c7cd8ee04558db3faf
luzialeticia <leticialuzia@tutamail.com>
2020-09-26 12:12:25 -0300

    Criando o objeto newMusicList

--- src/controller/musicController.js
+++ src/controller/musicController.js
@@ -1,13 +1,21 @@
 const { request, response } = require("../app")
 const musics = require("../model/musics.json")
 
+const newMusicList = musics.map(music => {
+    const newMusic = {
+        id: music.id,
+        nome: music.name,
+        amostra: music.preview_url,
+        nome_album: music.album.name,
+        imagem: music.album.url,
+        artista: music.artists.name
+    }
+    return newMusic
+})
+
 const getMusics = (request, response) => {
     console.log(request.url)
-    response.status(200).send(musics)
-}
-
-const newMusicList = (request, response) => {
-
+    response.status(200).send(newMusicList)
 }
 
 module.exports = {
