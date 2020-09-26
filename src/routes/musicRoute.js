8e11a17b4fdaea1bdba03fc34b5498576df0f1a4
luzialeticia <leticialuzia@tutamail.com>
2020-09-26 14:31:55 -0300

    Editando rotas

--- src/routes/musicRoute.js
+++ src/routes/musicRoute.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const controller = require("../controller/musicController")
 
-router.get("/", controller.getMusics)
-router.get("/:id", controller.getMusicById)
+router.get("/musics", controller.getMusics)
+router.get("/musics/:id", controller.getMusicById)
 
 module.exports = router
