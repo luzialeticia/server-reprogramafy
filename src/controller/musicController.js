fe1c479681c16975441f7d6107b7e16427d3f42f
luzialeticia <leticialuzia@tutamail.com>
2020-09-26 11:58:00 -0300

    Troquei de getAll para getMusics

--- src/controller/musicController.js
+++ src/controller/musicController.js
@@ -1,11 +1,15 @@
 const { request, response } = require("../app")
 const musics = require("../model/musics.json")
 
-const getAll = (request, response) =>{
+const getMusics = (request, response) => {
     console.log(request.url)
     response.status(200).send(musics)
 }
 
+const newMusicList = (request, response) => {
+
+}
+
 module.exports = {
-    getAll
+    getMusics
 }
