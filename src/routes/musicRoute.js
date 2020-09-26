35920f96e2c2ea2e0be56f40bea07e134c1912b8
luzialeticia <leticialuzia@tutamail.com>
2020-09-26 14:49:27 -0300

    Criando a rota que busca artista

--- src/routes/musicRoute.js
+++ src/routes/musicRoute.js
@@ -4,5 +4,6 @@ const controller = require("../controller/musicController")
 
 router.get("/musics", controller.getMusics)
 router.get("/musics/:id", controller.getMusicById)
+router.get("/musics/buscar/:artist", controller.getArtist)
 
 module.exports = router
