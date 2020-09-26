0cea239c05a83b9b5cae4788785dc807a49a5cbb
luzialeticia <leticialuzia@tutamail.com>
2020-09-26 14:17:12 -0300

    Criando a rota pra procurar pro id

--- src/routes/musicRoute.js
+++ src/routes/musicRoute.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const controller = require("../controller/musicController")
 
-router.get("/musics", controller.getMusics)
+router.get("/", controller.getMusics)
+router.get("/:id", controller.getMusicById)
 
 module.exports = router
