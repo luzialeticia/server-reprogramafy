cd42cada537dec79325d0770b6d6e725f09f78e1
luzialeticia <leticialuzia@tutamail.com>
2020-09-26 11:58:26 -0300

    Editei a rota de acordo com o novo nome getMusics

--- src/routes/musicRoute.js
+++ src/routes/musicRoute.js
@@ -2,6 +2,6 @@ const express = require("express")
 const router = express.Router()
 const controller = require("../controller/musicController")
 
-router.get("/music", controller.getAll)
+router.get("/music", controller.getMusics)
 
 module.exports = router
